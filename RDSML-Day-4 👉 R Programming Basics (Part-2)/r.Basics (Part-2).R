915456db6e7797bba6b7cff8e6b161f38903f3e3
KaziRukan <rukantn24@gmail.com>
2025-05-07 22:50:38 +0600

    Day - 4 Practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/r.Basics (Part-2).R
@@ -0,0 +1,48 @@
+# Data Types in R
+# 1.Numreic - Integer / Floating Point
+# 2. Sting/character - "Kazi Rukan" / 'Kazi Rukan'
+# 3. Logical
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "Kazi Rukan"
+print(name)
+class(name)
+
+c = "52"
+print(c + 9)
+print(a + 9)
+class(c)
+
+# Logical Data Types  TRUE, FALSE (T, F)
+class(a > b)
+x = 19
+y = 22-3
+print(x==y)
+print(x!=y)
+print(y > x)
+print(y >= x)
+# Logical Operators in R
+# && Logical Operator
+# || Logical Operator
+
+4 == 4 && 5 == 5
+x != y || x == y
+
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * (2 /3)
+print(z)
+
+w = (4 + 3 - 3) * 2 / 3
+print(x)
+
+y = 4^3
+print(y)
+
+## Module Operator
+11 / 5
+11 %% 5
+11 %/% 5
