e70161c1b711d70c5c7f55fdf54901f4968a2c38
KaziRukan <rukantn24@gmail.com>
2025-05-31 22:39:21 +0600

    Day_11

---
+++ RDSML-Day-11 👉 Scatter Plot in R (Part-2)/scatter.R
@@ -0,0 +1,49 @@
+install.packages("ggplot2")
+library(ggplot2)
+
+dataset = mtcars
+
+# Generating a basic scatter plot
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# Changing the point size and shape
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18)
+
+# Adding the regression line
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18) +
+  geom_smooth(method = 1m)
+
+# Removing the Confidence Interval
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE)
+
+# loess method
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth()
+
+# Change the line type and color
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18) +
+  geom_smooth(method = lm, linetype = "dashed",
+              color = "darkblue")
+
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18) +
+  geom_smooth(method = lm, linetype = "dotted",
+              color = "darkblue")
+
+# Changing the confidence interval color
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18, color = "blue") +
+  geom_smooth(method = lm, linetype = "dashed",
+              color = "darkred", fill = "blue")
+
+# Scatter plots with multiple groups
+
+# Changing the point shape 
+
